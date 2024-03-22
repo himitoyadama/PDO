3f22fb925f9ad439cdff481231f06ff694471a96
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2024-03-22 09:29:16 +0900

    Update create_tb.sql

--- create_tb.sql
+++ create_tb.sql
@@ -3,7 +3,7 @@ create database dbname;
 use dbname;
 
 create table tbname (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    customer_id VARCHAR(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     customer_name VARCHAR(50),
     point INT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
