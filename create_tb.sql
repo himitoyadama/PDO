38c41c720b71b5c0b090801eb699195122a3a1b0
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2024-03-22 12:02:42 +0900

    Update create_tb.sql

--- create_tb.sql
+++ create_tb.sql
@@ -2,8 +2,9 @@ create database dbname;
 
 use dbname;
 
+//varcharをprimary keyにする場合auto_incrementは書かない
 create table tbname (
-    customer_id VARCHAR(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    customer_id VARCHAR(5) NOT NULL PRIMARY KEY,
     customer_name VARCHAR(50),
     point INT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
